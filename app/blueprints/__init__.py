a9d54d43705b66ed6ec664bc1da7921bca65cc05
ken <yuanchengjie@qimengkeji.com>
2025-06-18 15:20:38 +0800

    删除 收入分析 相关页面以及路由

--- app/blueprints/__init__.py
+++ app/blueprints/__init__.py
@@ -7,11 +7,9 @@ This package contains all Flask blueprints for the application.
 from .main import main_bp
 from .transactions import transactions_bp
 from .settings import settings_bp
-from .income import income_bp
 
 __all__ = [
     'main_bp',
     'transactions_bp',
     'settings_bp',
-    'income_bp'
 ]
